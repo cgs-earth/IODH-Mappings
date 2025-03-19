6d1fa37621a14423a52740a7f94af70f6a65fc75
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-03-19 09:41:19 -0400

    response to feedback

--- rise/lib/geojson/types.py
+++ rise/lib/geojson/types.py
@@ -6,7 +6,9 @@ class GeojsonFeature(TypedDict):
     properties: dict
     id: int
 
-class GeojsonResponseDict(TypedDict):
+class GeojsonFeatureCollectionDict(TypedDict):
+    """A dict reprsentation of a GeoJSON FeatureCollection that is returned from OAF items/ queries"""
     type: Literal["FeatureCollection"]
     features: list[GeojsonFeature]
+    # numberMatched is not required because it is only returned when resulttype=hits
     numberMatched: NotRequired[int]
--- rise/lib/location.py
+++ rise/lib/location.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, field_validator
 import shapely
 import shapely.wkt
 from rise.env import TRACER
-from rise.lib.geojson.types import GeojsonResponseDict
+from rise.lib.geojson.types import GeojsonFeatureCollectionDict
 from rise.lib.helpers import (
     merge_pages,
     no_duplicates_in_pages,
@@ -242,7 +242,7 @@ class LocationResponse(BaseModel):
             str, dict[Literal["type"], Literal["number", "string", "integer"]]
         ] = {},
         sortby: Optional[list[SortDict]] = None,  # now treat as list[SortDict]
-    ) -> GeojsonResponseDict:
+    ) -> GeojsonFeatureCollectionDict:
         """
         Convert a list of locations to geojson
         """
@@ -332,7 +332,7 @@ class LocationResponse(BaseModel):
         validated_geojson = FeatureCollection(
             type="FeatureCollection", features=geojson_features
         )
-        return GeojsonResponseDict(**validated_geojson.model_dump(by_alias=True))
+        return GeojsonFeatureCollectionDict(**validated_geojson.model_dump(by_alias=True))
 
 class LocationResponseWithIncluded(LocationResponse):
     """
